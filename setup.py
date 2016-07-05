e46d9cc6f8dc076e9dc86f6f8511fc6f3aa95f6e
Alex <alexprengere@gmail.com>
2016-07-05 02:39:12 -0400

    Adding LICENSE

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import with_statement
+
 from setuptools import setup
+
+
+with open('README.rst') as f:
+    LONG_DESCRIPTION = f.read()
+
+with open('LICENSE') as f:
+    LICENSE = f.read()
 
 setup(
     name='FormalSystems',
-    version='0.1.2',
-    description='Play with formal systems from "Gödel Escher Bach", Douglas Hofstadter',
+    version='0.1.3',
     author='Alex Prengère',
     author_email='alexprengere@gmail.com',
     url='http://alexprengere.github.com/FormalSystems',
+    description='Play with formal systems from "Gödel Escher Bach", Douglas Hofstadter',
+    long_description=LONG_DESCRIPTION,
+    license=LICENSE,
     packages=[
         'formalsystems',
     ],
